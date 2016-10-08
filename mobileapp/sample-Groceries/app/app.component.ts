16f3d8683c859534cfe443dbc2558dad84403027
faunus <faunotv@gmail.com>
2016-10-08 17:48:43 -0500

    + NS styles && 1-way binding

--- mobileapp/sample-Groceries/app/app.component.ts
+++ mobileapp/sample-Groceries/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from "@angular/core";
 
 @Component({
+  moduleId: module.id,
   selector: "my-mobile-app",
+  styleUrls: ['pages/login/login-common.css', 'pages/login/login.css'],
   template: `
   <StackLayout>
-    <TextField hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none"></TextField>
+    <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+    <TextField hint="Email Address" keyboardType="email" [text]="email"
+  autocorrect="false" autocapitalizationType="none"></TextField>
     <TextField hint="Password" secure="true"></TextField>
 
-    <Button text="Sign in"></Button>
+    <Button class="submit-button" text="Sign in" (tap)="Submit()"></Button>
     <Button text="Sign up for Groceries"></Button>
   </StackLayout>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  email = "nativescriptrocks@telerik.com";
+  Submit() {
+    alert(`hello::${this.email}`);
+  }
+}
