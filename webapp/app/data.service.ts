dd4d815f4b758bcadd5612ddd833edb2258c283f
faunus <faunotv@gmail.com>
2016-10-08 02:29:44 -0500

    + webapp dir

--- webapp/app/data.service.ts
+++ webapp/app/data.service.ts
@@ -28,8 +28,8 @@ export class HeroService {
     constructor(private http: Http) { }
 
     private handleError(error: any): Promise<any> {
-        console.error('HeroService::An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+        console.error('HeroService::An error occurred', error) // for demo purposes only
+        return Promise.reject(error.message || error)
     }
 
     private headers = new Headers({ 'Content-Type': 'application/json' });
